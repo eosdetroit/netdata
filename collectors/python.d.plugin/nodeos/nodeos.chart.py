7da79a17b9520b9cd78f5b3eb12e6863b4415be4
bitcoinerS <bitcoiner@cryptomoon.com>
2019-01-23 19:08:02 +0700

    wip nodeos plugin and dashboard

--- collectors/python.d.plugin/nodeos/nodeos.chart.py
+++ collectors/python.d.plugin/nodeos/nodeos.chart.py
@@ -1,49 +1,72 @@
 # -*- coding: utf-8 -*-
-# Description: example netdata python.d module
+# Description: nodeos netdata python.d module
 # Author: bitcoiners
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from random import SystemRandom
+from bases.FrameworkServices.UrlService import UrlService
 
-from bases.FrameworkServices.SimpleService import SimpleService
-
-
-priority = 90000
 
 ORDER = [
-    'random',
+    'connections',
+    'requests',
+    'sync_status',
+    'connect_rate',
 ]
 
 CHARTS = {
-    'random': {
-        'options': [None, 'A random number', 'random number', 'random', 'random', 'line'],
+    'connections': {
+        'options': [None, 'Active Connections', 'connections', 'active connections',
+                    'nodeos.connections', 'line'],
         'lines': [
-            ['random1']
+            ['active']
+        ]
+    },
+    'requests': {
+        'options': [None, 'Requests', 'requests/s', 'requests', 'nodeos.requests', 'line'],
+        'lines': [
+            ['requests', None, 'incremental']
+        ]
+    },
+    'sync_status': {
+        'options': [None, 'Active Connections by Status', 'connections', 'status',
+                    'nodeos.sync_status', 'line'],
+        'lines': [
+            ['reading'],
+            ['writing'],
+            ['waiting', 'idle']
+        ]
+    },
+    'connect_rate': {
+        'options': [None, 'Connections Rate', 'connections/s', 'connections rate',
+                    'nodeos.connect_rate', 'line'],
+        'lines': [
+            ['accepts', 'accepted', 'incremental'],
+            ['handled', None, 'incremental']
         ]
     }
 }
 
 
-class Service(SimpleService):
+class Service(UrlService):
     def __init__(self, configuration=None, name=None):
-        SimpleService.__init__(self, configuration=configuration, name=name)
+        UrlService.__init__(self, configuration=configuration, name=name)
         self.order = ORDER
         self.definitions = CHARTS
-        self.random = SystemRandom()
+        self.url = self.configuration.get('url')
 
-    @staticmethod
-    def check():
-        return True
-
-    def get_data(self):
-        data = dict()
-
-        for i in range(1, 4):
-            dimension_id = ''.join(['random', str(i)])
-
-            if dimension_id not in self.charts['random']:
-                self.charts['random'].add_dimension([dimension_id])
-
-            data[dimension_id] = self.random.randint(0, 100)
-
-        return data
+    def _get_data(self):
+        """
+        Format data received from http request
+        :return: dict
+        """
+        try:
+            raw = self._get_raw_data().split(" ")
+            return {'active': int(raw[2]),
+                    'requests': int(raw[9]),
+                    'reading': int(raw[11]),
+                    'writing': int(raw[13]),
+                    'waiting': int(raw[15]),
+                    'accepts': int(raw[7]),
+                    'handled': int(raw[8])}
+        except (ValueError, AttributeError):
+            return None
